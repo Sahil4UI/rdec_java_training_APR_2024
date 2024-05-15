bc32f67c760f4990101810f5292220f24bdffd86
Sahil Kumar <54834362+Sahil4UI@users.noreply.github.com>
2024-05-15 10:18:10 +0530

    Update TicTacToeDemo1.java

--- TicTacToeDemo1.java
+++ TicTacToeDemo1.java
@@ -1,22 +1,32 @@
 import java.util.Scanner;
 
-interface Game{
-	void gameBoard();
-	String playerMove(String choice,int pos);
-	void cpuMove(String cpu_choice);
+interface MyGame{
+	void showMatrix();
+	
+	String player_turn();
+	
+	void cpu_turn();
+	
 	String winner(String choice);
 }
 
-class TicTacToe1 implements Game{
-	String x[] =
-		{"1","2","3","4","5","6","7","8","9"};
-	
-	int win_combs[][] = {{0,1,2},{3,4,5},{6,7,8},
-			{0,3,6},{1,4,7},{2,5,8},
-			{0,4,8},{2,4,6}};
+class Game implements MyGame{
 
+	String x[] = {"1","2","3","4","5","6","7","8","9"}; 
+	int wincomb[][] = {{0,1,2},{3,4,5},{6,7,8},
+						{0,3,6},{1,4,7},{2,5,8},
+						{0,4,8},{2,4,6}};
+	
+	String user_choice,cpu_choice;
+	int cpu_pos,my_pos;
+	
+	Game(String user_choice,String cpu_choice)
+	{
+		this.user_choice = user_choice;
+		this.cpu_choice = cpu_choice;
+	}
 	@Override
-	public void gameBoard() {
+	public void showMatrix() {
 		// TODO Auto-generated method stub
 		for (int i=0;i<x.length;i++)
 		{
@@ -33,64 +43,59 @@ class TicTacToe1 implements Game{
 	}
 
 	@Override
-	public String playerMove(String choice, int pos) {
+	public String player_turn() {
 		// TODO Auto-generated method stub
-		x[pos-1] = choice;
-		gameBoard();
-		return winner(choice);
+		x[my_pos-1] = user_choice;
+		showMatrix();
+		return winner(user_choice);
+		
 	}
 
 	@Override
-	public void cpuMove(String cpu_choice) {
-		System.out.println("CPU TURN");
-		int cpu_pos=(int)(Math.random()*9);
-		x[cpu_pos] = cpu_choice;
+	public void cpu_turn() {
+		// TODO Auto-generated method stub
 		
-		gameBoard();
 	}
 
 	@Override
 	public String winner(String choice) {
 		// TODO Auto-generated method stub
-		for (int arr[] : win_combs)
+		for (int arr[] : wincomb)
 		{
-			if (x[arr[0]]==choice 
-					&& x[arr[1]]==choice 
-					&& x[arr[2]]==choice)
+			if (x[arr[0]]==choice && 
+				x[arr[1]]==choice &&
+				x[arr[2]]==choice)
 			{
 				return choice;
 			}
 		}
-		
-		return "";
-		
+		return null;
 	}
 	
 }
 
-
-public class TicTacToeDemo1 {
-
+public class TicTacToe {
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		TicTacToe1 obj = new TicTacToe1();
-		String cpu_choice,my_choice;
-		Scanner sc = new Scanner(System.in);
-		System.out.print("\nEnter Choice (X/0):");
-		my_choice = sc.next().toUpperCase();
-		cpu_choice = (my_choice.equals("X"))?"0":"X";
-		obj.gameBoard();
+		
+		Scanner sc =  new Scanner(System.in);
+		String player_choice,cpu_choice;
+		System.out.println("Enter Choice X/0 : ");
+		player_choice = sc.nextLine().toUpperCase();
+		cpu_choice = (player_choice=="X")?"0":"X";
+		Game mygame = new Game(player_choice,cpu_choice);
+		mygame.showMatrix();
 		while (true)
 		{
-			System.out.println("Enter Pos b/w (1-9)");
-			int pos = sc.nextInt();
-			String r = obj.playerMove(my_choice, pos);
-			if (r==my_choice)
+			System.out.println("Enter Pos 1-9 : ");
+			mygame.my_pos=sc.nextInt();
+			String res = mygame.player_turn();
+			if (res==player_choice)
 			{
-				System.out.println(r+" is Winner");
+				System.out.println("Player Wins");
 				break;
 			}
-			obj.cpuMove(cpu_choice);
 		}
 
 	}
